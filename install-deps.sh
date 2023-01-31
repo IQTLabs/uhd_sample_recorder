aa516ba13e5510198e9319a5a172da2dbebada28
Josh Bailey <josh@vandervecken.com>
2023-01-31 19:20:07 +0000

    hotfix for glslang branch rename.

--- install-deps.sh
+++ install-deps.sh
@@ -2,5 +2,6 @@
 
 sudo apt-get update && sudo apt-get install -qy build-essential cmake libuhd-dev libboost-all-dev cppcheck libarmadillo-dev unzip wget libvulkan-dev valgrind && \
   wget https://sourceforge.net/projects/sigpack/files/sigpack-1.2.7.zip -O sigpack.zip && unzip sigpack.zip && ln -s sigpack-*/sigpack . && \
-  git clone https://github.com/DTolm/VkFFT -b v1.2.31 && \
+  git clone https://github.com/DTolm/VkFFT && \
+  cd VkFFT && git checkout 7113de9abc16f7411412e7e1137fc8d0beebd847 && patch < ../vkfft-main-patch.txt && cd .. && \
   wget https://github.com/nlohmann/json/releases/download/v3.11.2/json.hpp
