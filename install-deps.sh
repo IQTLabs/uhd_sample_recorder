5482e9d345011b79b80ff964253a5a169ada56dd
Josh Bailey <josh@vandervecken.com>
2023-03-12 20:30:52 +0000

    no longer need patch for branch name workaround.

--- install-deps.sh
+++ install-deps.sh
@@ -3,5 +3,5 @@
 sudo apt-get update && sudo apt-get install -qy build-essential cmake libuhd-dev libboost-all-dev cppcheck libarmadillo-dev unzip wget libvulkan-dev valgrind && \
   wget https://sourceforge.net/projects/sigpack/files/sigpack-1.2.7.zip -O sigpack.zip && unzip sigpack.zip && ln -s sigpack-*/sigpack . && \
   git clone https://github.com/DTolm/VkFFT && \
-  cd VkFFT && git checkout 7113de9abc16f7411412e7e1137fc8d0beebd847 && patch < ../vkfft-main-patch.txt && cd .. && \
+  cd VkFFT && git checkout 41a48088b50d8079a2b54b67f77099f665b3b8c2 && cd .. && \
   wget https://github.com/nlohmann/json/releases/download/v3.11.2/json.hpp
