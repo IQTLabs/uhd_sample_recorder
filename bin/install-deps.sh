ee6e57c753576945bf82474d1ec6c03931d6fe8a
Josh Bailey <josh@vandervecken.com>
2023-09-06 22:46:36 +0000

    fix vkfft build breakage due to glslang change.

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
-sudo apt-get update && sudo apt-get install -qy build-essential cmake libuhd-dev libboost-all-dev cppcheck libarmadillo-dev unzip wget libvulkan-dev valgrind && \
+sudo apt-get update && sudo apt-get install -qy \
+  build-essential \
+  cmake \
+  cppcheck \
+  libarmadillo-dev \
+  libboost-all-dev \
+  libuhd-dev \
+  libvulkan-dev \
+  unzip \
+  valgrind \
+  wget \
+  && \
   wget https://sourceforge.net/projects/sigpack/files/sigpack-1.2.7.zip -O sigpack.zip && unzip sigpack.zip && ln -s sigpack-*/sigpack . && \
+  wget https://github.com/nlohmann/json/releases/download/v3.11.2/json.hpp && \
   git clone https://github.com/DTolm/VkFFT -b v1.3.1 && \
-  wget https://github.com/nlohmann/json/releases/download/v3.11.2/json.hpp
+  git clone https://github.com/KhronosGroup/SPIRV-Tools -b v2023.2 && \
+  cd SPIRV-Tools/external && \
+  git clone https://github.com/KhronosGroup/SPIRV-Headers -b sdk-1.3.246.1 spirv-headers && \
+  cd ../..
