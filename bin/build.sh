de6c9f26528bfe7c4bf17ffe2d4e66cb475f589a
Josh Bailey <josh@vandervecken.com>
2023-10-18 22:28:21 +0000

    SPIRV tools generates broken code on ARM64.

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
-cd SPIRV-Tools && mkdir build && cd build && CMAKE_BUILD_TYPE=Release cmake .. && make -j $(nproc) && sudo make install && cd ../..
-cd VkFFT && mkdir build && cd build && CMAKE_BUILD_TYPE=Release cmake -DALLOW_EXTERNAL_SPIRV_TOOLS=ON .. && make -j $(nproc) && cd ../.. && \
+cd VkFFT && mkdir build && cd build && CMAKE_BUILD_TYPE=Release cmake .. && make -j $(nproc) && cd ../.. && \
   mkdir build && cd build && cmake ../lib && make -j $(nproc) && cd ..
