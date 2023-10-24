824c772d9c1db86b04b79a5c0cb4c6c432e4f334
Josh Bailey <josh@vandervecken.com>
2023-10-24 20:25:34 +0000

    VkFFT 1.3.2.

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-cd VkFFT && mkdir build && cd build && CMAKE_BUILD_TYPE=Release cmake .. && make -j $(nproc) && cd ../.. && \
+cd VkFFT && mkdir build && cd build && CMAKE_BUILD_TYPE=Release cmake -DGLSLANG_GIT_TAG=13.0.0 .. && make -j $(nproc) && cd ../.. && \
   mkdir build && cd build && cmake ../lib && make -j $(nproc) && cd ..
